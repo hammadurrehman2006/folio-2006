103817e13cd30ab0e62a72143adf83b13ce04ba6
m_hammad-ur-rehman <hammadurrehman7707@gmail.com>
2024-12-03 21:33:31 +0500

    Update Layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
     <html lang="en" >
-      <body className={`${inter.className} bg-black text-white`}>
+      <body className={`${inter.className}`}>
           {children}
         
       </body>
--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-//import { ModeToggle } from "../subcomponents/Toggle";
 
 export default function Navbar() {
   return (
