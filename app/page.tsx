dc85af342b2241da1dd2571afa5c969d23f0a9db
m_hammad-ur-rehman <hammadurrehman7707@gmail.com>
2024-12-03 21:24:27 +0500

    Update global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 import Hero from "../components/ui/Hero"
 import Navbar from "../components/global/Navbar";
 import Footer from "../components/global/Footer";
-// import { renderCanvas } from './components/Render';
-// import { useEffect } from "react";
 export default function Home() {
-  // useEffect(() => {
-  //   renderCanvas();
-  // }, []);
   return (
     <>
       <Navbar />
